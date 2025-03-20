7257ff974dfaebbc60a4ecfe802321a615c18ea4
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-20 11:24:33 -0700

    Completed program 1 of assignment

--- Week 10 Assignments/main.cpp
+++ Week 10 Assignments/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-static int pigeonHole() {
-
-
-    return 0;
+static int pigeonHole(int objects, int groups) {
+    return objects * groups + 1;
 }
 
 static int combination() {
@@ -15,7 +13,8 @@ static int combination() {
 int main() {
     std::cout << "Eisig Liang - 23 Mar. 2025\n";
 
-
+    // Testing pigeonHole; used objects = 3 - 1 = 2 students and groups = 12 months
+    std::cout << "Min. num. of students in class s.t. 3 are born in the same month: " << pigeonHole(3 - 1, 12) << std::endl;
 
     return 0;
 }
