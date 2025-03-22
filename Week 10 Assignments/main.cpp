e471226c19d3e744af88b0087f93e5fdc3ac6bc7
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-21 18:22:23 -0700

    Completed 2nd program

--- Week 10 Assignments/main.cpp
+++ Week 10 Assignments/main.cpp
@@ -4,10 +4,18 @@ static int pigeonHole(int objects, int groups) {
     return objects * groups + 1;
 }
 
-static int combination() {
+static int factorial(int n) {
+    int result = n;
+    n--;
 
+    for (n; n > 0; n--) 
+        result *= n;
 
-    return 0;
+    return result;
+}
+
+static int combination(int n, int r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
 }
 
 int main() {
@@ -15,6 +23,9 @@ int main() {
 
     // Testing pigeonHole; used objects = 3 - 1 = 2 students and groups = 12 months
     std::cout << "Min. num. of students in class s.t. 3 are born in the same month: " << pigeonHole(3 - 1, 12) << std::endl;
+
+    // Testing combination; used C(6, 3) * C(5, 2) * C(8, 4)
+    std::cout << "Num. of choices farmer 1 can choose from farmer 2's stock: " << combination(6, 3) * combination(5, 2) * combination(8, 4);
 
     return 0;
 }
